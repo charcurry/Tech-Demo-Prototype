46976794b820713ab5b0817add106c1aeefbf630
Charlie <charlieryancurry@gmail.com>
2024-02-09 11:44:46 -0400

    fix moving platforms

--- Assets/Scripts/Moving Platform.cs
+++ Assets/Scripts/Moving Platform.cs
@@ -8,6 +8,9 @@ public class MovingPlatform : MonoBehaviour
     private GameObject[] positions;
     public int destination;
 
+    public float deceleration;
+    public float acceleration;
+
     public float moveSpeed;
 
     // Start is called before the first frame update
@@ -24,17 +27,25 @@ public class MovingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, positions[destination].transform.position, moveSpeed * Time.deltaTime);
+        float distance = Vector3.Distance(positions[destination].transform.position, transform.position);
 
-        if (Vector3.Distance(positions[destination].transform.position, transform.position) == 0)
+        if (distance < 0.01f)
         {
             destination++;
+            moveSpeed = 0;
         }
-        if (destination == positions.Length)
+
+        if (destination >= positions.Length)
         {
             positions.Reverse();
             destination = 0;
         }
+
+        moveSpeed = Mathf.Lerp(moveSpeed, 1.0f, Time.deltaTime * acceleration);
+
+        float t = 1 - Mathf.Exp(-moveSpeed * Time.deltaTime * deceleration);
+
+        transform.position = Vector3.Lerp(transform.position, positions[destination].transform.position, t);
     }
 
     private void OnTriggerEnter(Collider other)
